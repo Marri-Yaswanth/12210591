48a915022b98087d8a0e5c81fbb24537409c0203
MarriYaswanth <yashumarri24@gmail.com>
2025-07-14 11:18:01 +0530

    Completed logging.ts code

---
+++ logging middleware/logging.ts
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const Log_Endpoint = 'http://20.244.56.144/evaluation-service/logs'
+
+const Allowed_Stacks = ['backend' , 'frontend']
+const Allowed_Levels = ['debug' , 'info', 'warn', 'error' , 'fatal']
+const Allowed_Packages = [
+    'cache',
+    'controller',
+    'cron job',
+    'db',
+    'domain',
+    'handler',
+    'repository'
+]
+
+const Auth_Token = process.env.Logging_Api_Token || " "
+
+interface Log_Params {
+    stack: 'backend' | 'frontend',
+    level: 'debug' | 'info' | 'warn' | 'error' | 'fatal',
+    package: typeof Allowed_Packages[number],
+    message: string,
+}
+
+export async function log({stack , level , package: pkg , message}: Log_Params):
+Promise<void>{
+    try{
+        if(!Allowed_Stacks.includes(stack)){
+            throw new Error(`Stack "${stack}" is not allowed.`)
+        }
+        if(!Allowed_Levels.includes(level)){
+            throw new Error(`Level "${level}" is not allowed.`)
+        }
+        if(!Allowed_Packages.includes(pkg)){
+            throw new Error(`Package "${pkg}" is not allowed.`)
+        }
+        
+        await axios.post(
+            Log_Endpoint,
+            {
+                stack,
+                level,
+                package: pkg,
+                message
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${Auth_Token}`
+                },
+            }
+        )
+    }catch(err){
+        console.log('Logging failed: ' + err.message)
+    }
+}
